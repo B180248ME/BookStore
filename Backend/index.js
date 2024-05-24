2a3585a63ec6f041259f6170935e8a95b757a10d
B180248ME <bijjakavya1999@gmail.com>
2024-05-24 15:21:37 -0700

    added CORS POLICY to add or restrict the access to origin

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,7 @@ import { PORT, MongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
 import router from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 // Like Middleware to parse request body in json
@@ -12,8 +13,18 @@ app.get('/',(request,response)=>{
     console.log("MERN STACK");
     return response.status(245).send("Message with Http");
 })
-
+// To access all the routes 
 app.use('/books',router);
+
+// Like a Middleware to handle CORS POLICY
+// Option 1: Allow all Origins with default values of Cors(*)
+// app.use(cors)
+// Option 2: Allow Custom Origins
+app.use(cors({
+    origin: 'http://localhost:3000/',
+    methods: ['GET','POST','PUT',"DELETE"],
+    allowedHeaders: ['Content-Type']
+}))
 
 // Connect to MongoDB database
 mongoose

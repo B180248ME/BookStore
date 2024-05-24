8d1e7660755e71537528c13fe58a3135e5c222fb
B180248ME <bijjakavya1999@gmail.com>
2024-05-24 13:53:25 -0700

    Book Model created and /books route created to add books to database

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 import express, { request, response } from "express";
 import { PORT, MongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
 app.get('/',(request,response)=>{
@@ -8,11 +9,32 @@ app.get('/',(request,response)=>{
     return response.status(245).send("Message with Http");
 })
 
+// For New Book
+app.post('/books',async(request,response)=>{
+    try{
+        if(
+            !request.body.title || request.body.author || request.body.publishYear
+        ) {
+            return response.status(400).send({message:'Please give all the requied fields: title, author, publishYear'})
+        }
+        const newBook = {
+            title:request.body.title,
+            author:request.body.author,
+            publishYear:request.body.publishYear
+        }
+        const book = await Book.create(newBook);
+        return response.status(201).send({message: "added book successfully"},book)
+    }
+    catch(error){
+        console.error("ERROR ADDING NEW BOOK!!" ,error.message);
+        return response.status(400).send({message:error.message});
+    }
+});
 
 mongoose
     .connect(MongoDBURL)
     .then(()=>{
-        console.log("app successfully connted to MongoDB database");
+        console.log("app successfully connected to MongoDB database");
         app.listen(PORT,()=>{
             console.log(`Listening to PORT : ${PORT}`);
         })
@@ -20,3 +42,5 @@ mongoose
     .catch((error)=>{
         console.error("ALERT ERROR !!" ,error);
     })
+
+

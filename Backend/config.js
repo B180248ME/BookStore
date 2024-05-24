9bdf3f5eff5639baa01685c7060d5673a6b4ad59
B180248ME <bijjakavya1999@gmail.com>
2024-05-23 18:57:19 -0700

    database MongoDB is connected that is hosted in aws from mongdb.com website

--- Backend/config.js
+++ Backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const MongoDBURL = 'mongodb+srv://rootuser:rootpass@bookstoredb.hcytwts.mongodb.net/?retryWrites=true&w=majority&appName=BookStoreDB'
--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,22 @@
 import express, { request, response } from "express";
-import { PORT } from "./config.js";
+import { PORT, MongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 app.get('/',(request,response)=>{
     console.log("MERN STACK");
     return response.status(245).send("Message with Http");
 })
-app.listen(PORT,()=>{
-    console.log(`Listening to PORT : ${PORT}`)
-})
+
+
+mongoose
+    .connect(MongoDBURL)
+    .then(()=>{
+        console.log("app successfully connted to MongoDB database");
+        app.listen(PORT,()=>{
+            console.log(`Listening to PORT : ${PORT}`);
+        })
+    })
+    .catch((error)=>{
+        console.error("ALERT ERROR !!" ,error);
+    })

7912e86e35363e0937bf45f74ceb3ddd10b08135
B180248ME <bijjakavya1999@gmail.com>
2024-06-09 18:31:46 -0700

    create books page redirection link added

--- Frontend/src/pages/CreateBooks.jsx
+++ Frontend/src/pages/CreateBooks.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Spinner from '../components/Spinner';
+import BackButton from '../components/BackButtom';
+import axios from 'axios';
 
 const CreateBooks = () => {
+    const [book, setBook] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const {id} = useParams();
+  
+    useEffect(()=>{
+      setLoading(true);
+      axios
+         .get(`http://localhost:5555/books/${id}`)
+         .then((response)=>{
+          setBook(response.data.book);
+          setLoading(false);
+         })
+         .catch((error)=>{
+          console.log("Error in loading the datails of book",error);
+          setLoading(false);
+         })
+    },[])
   return (
     <div>CreateBooks</div>
   )
